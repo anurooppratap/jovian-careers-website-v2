2651dd50a4bfe5e93d9ac079bdec3a7ba665e0f9
anurooppratap <anuroop.3.pratap@gmail.com>
2024-11-04 08:43:01 +0000

    added html home page and some static assets (images)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # from re import DEBUG
-from flask import Flask 
+from flask import Flask, render_template 
 
 # flask -> module
 # Flask -> class
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
