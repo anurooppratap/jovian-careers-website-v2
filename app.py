6d00cf176c3cde323236659f66c6ce9d25baeea2
anurooppratap <anuroop.3.pratap@gmail.com>
2024-11-04 08:11:33 +0000

    added the flask project

---
+++ app.py
@@ -0,0 +1,15 @@
+# from re import DEBUG
+from flask import Flask 
+
+# flask -> module
+# Flask -> class
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+  
+  
